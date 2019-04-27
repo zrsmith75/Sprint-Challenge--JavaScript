e8750982055fa86ab529105781d43a8faaf126ff
Zac Smith <zrs3141592@gmail.com>
2019-04-27 12:33:36 -0600

    finished regular arrays

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -51,7 +51,8 @@ console.log(stegosaurus.length);
 console.log(tyrannosaurus.period);
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+console.log(tyrannosaurus.roar());
+
 
 
 // ==== Arrays ====
@@ -70,9 +71,14 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 {"id":10,"first_name":"Hube","university":"Universitat Rovira I Virgili Tarragona","email":"hlethbrig9@foxnews.com"}]
 
 /* Request 1: Create a new array called universities that contains all the univeristies in the graduates array.  
-
 Once you have the new array created, sort the universities alphabetically and log the result. */
+
 const universities = [];
+
+for(let i = 0; i < graduates.length; i++) {
+  universities.push(graduates[i].university);
+}
+universities.sort();
 console.log(universities)
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
@@ -81,12 +87,24 @@ The resulting contact information should have a space between the first name and
 Name email@example.com
 
 Log the result of your new array. */
+
 const contactInfo = [];
+for(let i = 0; i < graduates.length; i++) {
+  contactInfo.push(`First name: ${graduates[i].first_name} Email: ${graduates[i].email}`);
+}
 console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
 const uni = [];
+
+for(let i =0; i < graduates.length; i++) {
+  
+  if (graduates[i].university.toLowerCase().includes("uni")) {
+    uni.push(graduates[i].university);
+  };
+}
+
 console.log(uni);
 
 
